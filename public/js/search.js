a97dc0d9c7fca95af3951fd19688ad9ee8bb5470
Anastaisa <anastas.tarasenko001@mymdc.net>
2024-09-29 00:09:17 -0400

    fix how the call is made

--- public/js/search.js
+++ public/js/search.js
@@ -1,22 +1,24 @@
-document.getElementById('searchForm').addEventListener('submit', function(event) {
-    event.preventDefault(); // Prevent the form from submitting
-    const query = document.getElementById('searchInput').value;
-    searchpet(query); // Call the searchpet function with the input value
-});
-
-function searchpet(query) {
-    // Add your search logic here
-    console.log("Searching for:", query);
-    const words = query.split(' ');
-    console.log("Searching for:", words[0]);
-    const response = fetch('/test/test2?token=RtS0E2HxvBT5IKfFXRKWPTI97g0cTNnw5EkZ2&distance=' + words[0] +'&animal=' + words[1] + '&breed=' +words[2]+ '&location='+words[3], {
-        headers: { 'Content-Type': 'application/json' }
-    });
-
-    if (response.ok) {
-        document.location.replace('/test/test2?token=RtS0E2HxvBT5IKfFXRKWPTI97g0cTNnw5EkZ2&distance=' + words[0] +'&animal=' + words[1] + '&breed=' +words[2]+ '&location='+words[3]);
+const searchpet = async (event) => {
+    event.preventDefault();
+    const query = document.getElementById('searchInput').value.trim();
+    //adding logic to prevent blank request (will return everything from plant DB)
+    if (query == "") {
+        alert("Please enter at least one keyword")
     } else {
-        alert("responsen not ok")
+        let qJoined = query.split(' ').join('%20');
+        const response = await fetch('/test/test2?token=RtS0E2HxvBT5IKfFXRKWPTI97g0cTNnw5EkZ2&distance='
+            + qJoined, {
+            headers: { 'Content-Type': 'application/json' }
+        });
+
+        if (response.ok) {
+            document.location.replace('/test/test2?token=RtS0E2HxvBT5IKfFXRKWPTI97g0cTNnw5EkZ2&distance=' + qJoined);
+        } else {
+            alert("Error...")
+        }
     }
-    // You can add an AJAX request or any other logic to handle the search
-}
+};
+
+document
+    .getElementById('searchForm')
+    .addEventListener('submit', searchpet);
