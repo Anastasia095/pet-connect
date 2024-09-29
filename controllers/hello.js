1b077b948f1992ea20f6b024b89d6913000d9e2a
Anastaisa <anastas.tarasenko001@mymdc.net>
2024-09-29 03:36:48 -0400

    add one more page

--- controllers/hello.js
+++ controllers/hello.js
@@ -3,7 +3,12 @@ const router = express.Router();
 
 router.get('/', (req, res) => {
     console.log('Serving Hello Page');
-    res.render('hello', { name: 'Stranger', title: 'Hello Page' });
+    res.render('hello', { name: 'Stranger', title: 'Home' });
+});
+
+router.get('/getstarted', (req, res) => {
+    console.log('Serving get started Page');
+    res.render('getstarted', { name: 'Stranger', title: 'Get Started' });
 });
 
 module.exports = router;
